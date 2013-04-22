9414d7b039550f75a314757a56fea7560e1fcab7
Iara Ribeiro <Iara.prbr@gmail.com>
2013-04-22 15:14:19 -0300

    Arquivo dos indicadores de 2011 com a soma total de outliers e lista

--- eduBrasil/scripts/getSum.R
+++ eduBrasil/scripts/getSum.R
@@ -10,12 +10,40 @@ outliers <- data.frame(data2011$NOME_MUNICIPIO,data2011$OUTLIER_INDICADOR_329,da
 colnames(outliers) = c("NOME","INDICADOR_329","INDICADOR_62","INDICADOR_89","INDICADOR_90","INDICADOR_333", "INDICADOR_73",  "INDICADOR_74" ,
                        "INDICADOR_80",  "INDICADOR_81",  "INDICADOR_176", "INDICADOR_202","INDICADOR_184", "INDICADOR_7", "INDICADOR_201")
 
+data2011$sum = 0
 outliers$sum = 0
 
 for(i in 1:(nrow(data2011))){
   outliers[i,16] = sum(abs(outliers[i,2:15]), na.rm = T)
 }
 
+data2011$Outliers_Pos = NA
+data2011$Outliers_Neg = NA
 
-rowSums(outliers ==-1 & ! is.na(outliers))
-outliers[rowSums(outliers ==-1 & ! is.na(outliers)) > 0,]
+#rowSums(outliers ==-1 & ! is.na(outliers))
+
+lista_outliers = outliers[rowSums(outliers ==-1 & ! is.na(outliers)) > 0,]
+
+indicadores = colnames(outliers[2:15])
+
+for(i in 1:nrow(lista_outliers)){
+  outliers_positivos = ""
+  outliers_negativos = ""
+  
+  for(j in 2:16){
+    if(lista_outliers[i,j] == -1 & ! is.na(lista_outliers[i,j])){
+      outliers_negativos <- paste(outliers_negativos, indicadores[j],sep = " ")
+    }else if(lista_outliers[i,j] == 1 & ! is.na(lista_outliers[i,j])){
+      outliers_positivos <- paste(outliers_positivos, indicadores[j],sep = " ")
+    }
+  }
+  
+  lista_outliers[i,17] = substring(outliers_negativos,2,nchar(outliers_negativos))
+  lista_outliers[i,18] = substring(outliers_positivos,2,nchar(outliers_positivos))
+}
+
+data2011[is.element(data2011$NOME_MUNICIPIO, lista_outliers$NOME), ]$Outliers_Pos = lista_outliers$V18
+data2011[is.element(data2011$NOME_MUNICIPIO, lista_outliers$NOME), ]$Outliers_Neg = lista_outliers$V17
+data2011[is.element(data2011$NOME_MUNICIPIO, outliers$NOME), ]$sum = outliers$sum
+
+write.csv(data2011, "lista_outliers_2011.csv", col.names = T)
