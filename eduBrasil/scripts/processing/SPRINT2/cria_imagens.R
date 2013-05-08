a41db20ab9b267f566a45a21fe0ade952d13439f
bellezeta <bellezeta@gmail.com>
2013-05-08 11:29:00 -0300

    Adicionei os títulos dos gráficos da parte de cima.

--- eduBrasil/scripts/processing/SPRINT2/cria_imagens.R
+++ eduBrasil/scripts/processing/SPRINT2/cria_imagens.R
@@ -24,10 +24,11 @@ ano = 2011
 linha = desvios[desvios$NOME_MUNICIPIO==codigo & desvios$ANO==ano,]
 
 
-criaImagem = function(valor,indicador,codigo,dp){
+criaImagem = function(valor,indicador,codigo,dp,nomeIndicador){
 
 	#path = "C:/Users/Iara/Documents/GitHub/eduBrasil/eduBrasil/scripts/processing/SPRINT2/graphs/"
-	path = "C:/Users/Henrique/Desktop/eduBrasil/eduBrasil/scripts/processing/SPRINT2/graphs/"
+	path = "C:/Users/Henrique/Desktop/eduBrasil/eduBrasil/scripts/processing/SPRINT2/graphs/"  
+	#path = "/home/isa/git/eduBrasil/eduBrasil/scripts/processing/SPRINT2/"
 
 	desv = dp
 
@@ -35,7 +36,7 @@ criaImagem = function(valor,indicador,codigo,dp){
  		dp = dp*(-1)
 	}
 	png(paste(path ,codigo,"_",indicador,"_",desv,".png", sep=""),width = 382, height = 291)
-	barplot(valor[1], xlim=c(-0.5,2), ylim=c(0,max(valor)+dp), axes=T)
+	barplot(valor[1], xlim=c(-0.5,2), ylim=c(0,max(valor)+dp), axes=T, main=nomeIndicador)
 	abline(valor[2],0,col="green",lty=2) # MICRO
 	abline(valor[3],0,col="red",lty=4) # MESO
 	abline(valor[4],0,col="blue",lty=5) # ESTADO
@@ -43,6 +44,16 @@ criaImagem = function(valor,indicador,codigo,dp){
 	dev.off()
 }
 
+dicionario = data.frame(key = c(
+"INDICADOR_62", "INDICADOR_89", "INDICADOR_90", "INDICADOR_329", "INDICADOR_333", "INDICADOR_73", "INDICADOR_74", "INDICADOR_80", "INDICADOR_81","INDICADOR_176", "INDICADOR_177", "INDICADOR_202", "INDICADOR_184", "INDICADOR_7", "INDICADOR_201"), 
+indicador = c("Part. despesa e encargos educação(%)", "IDEB - 5º ano do ensino fundamental", "IDEB - 9º ano do ensino fundamental", 
+"Taxa de analfabetismo", "Taxa de atendimento escolar", "Taxa abandono total - fundamental(%)", "Taxa de abandono - ensino médio(%)",
+"Taxa aprovação total - fundamental(%)", "Taxa de aprovação - ensino médio (%)", "Percentual docentes formação superior(%)", 
+"Percentual de docentes temporários", "Índice precariedade infraestrutura", "Razão aluno por docente", "Despesa educação aluno", "Índice eficiência educação básica"))
+
+nomeIndicador = function(indicador){
+	dicionario[dicionario$key==indicador,]$indicador
+}
 
 for(i in seq(11, ncol(linha), 2)){
 
@@ -57,7 +68,7 @@ for(i in seq(11, ncol(linha), 2)){
 	
 	if(!is.na(valorIndicador)){
 		print(valorDesvio)
-		criaImagem(valor,colnames(desvios)[i],codigo,valorDesvio)
+		criaImagem(valor,colnames(desvios)[i],codigo,valorDesvio,nomeIndicador(colnames(desvios)[i]))
 	}
 
 }
