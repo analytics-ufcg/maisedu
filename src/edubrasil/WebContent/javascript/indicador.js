012e266c24cc330c83dc77b0e8d2582eea78798f
bellezeta <bellezeta@gmail.com>
2013-05-10 14:26:01 -0300

    Adicionei os nomes das cidades no dropdown.

--- src/edubrasil/WebContent/javascript/indicador.js
+++ src/edubrasil/WebContent/javascript/indicador.js
@@ -4,8 +4,7 @@ function plotIndicadores() {
 	var w = 600;
 	var h = 250;
 
-	var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15,
-	                20, 18, 17, 16, 18, 23, 25 ];
+	var dataset = [ 5, 10, 13, 19];
 
 	var xScale = d3.scale.ordinal().domain(d3.range(dataset.length))
 	.rangeRoundBands([ 0, w ], 0.05);
