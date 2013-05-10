eaa5a40741d13d503256708e479c30788f74f183
bellezeta <bellezeta@gmail.com>
2013-05-10 15:08:26 -0300

    Adicionei os botões de indicadores na parte de cima do gráfico.

--- src/edubrasil/WebContent/javascript/indicador.js
+++ src/edubrasil/WebContent/javascript/indicador.js
@@ -3,8 +3,70 @@ function plotIndicadores() {
 	//Width and height
 	var w = 600;
 	var h = 250;
+	
+	d3.csv("data.csv", function(error, data) {
+	  var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
 
+	  data.forEach(function(d) {
+	    d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
+	  });
+
+	  x0.domain(data.map(function(d) { return d.State; }));
+	  x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
+	  y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
+
+	  svg.append("g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(xAxis);
+
+	  svg.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis)
+	    .append("text")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 6)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end")
+	      .text("Population");
+
+	  var state = svg.selectAll(".state")
+	      .data(data)
+	    .enter().append("g")
+	      .attr("class", "g")
+	      .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
+
+	  state.selectAll("rect")
+	      .data(function(d) { return d.ages; })
+	    .enter().append("rect")
+	      .attr("width", x1.rangeBand()-10)
+	      .attr("x", function(d) { return x1(d.name); })
+	      .attr("y", function(d) { return y(d.value); })
+	      .attr("height", function(d) { return height - y(d.value); })
+	      .style("fill", function(d) { return color(d.name); });
+
+	  var legend = svg.selectAll(".legend")
+	      .data(ageNames.slice().reverse())
+	    .enter().append("g")
+	      .attr("class", "legend")
+	      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+	  legend.append("rect")
+	      .attr("x", width - 18)
+	      .attr("width", 18)
+	      .attr("height", 18)
+	      .style("fill", color);
+
+	  legend.append("text")
+	      .attr("x", width - 24)
+	      .attr("y", 9)
+	      .attr("dy", ".35em")
+	      .style("text-anchor", "end")
+	      .text(function(d) { return d; });
+
+	});
 	var dataset = [ 5, 10, 13, 19];
+		
 
 	var xScale = d3.scale.ordinal().domain(d3.range(dataset.length))
 	.rangeRoundBands([ 0, w ], 0.05);
@@ -76,14 +138,17 @@ function plotIndicadores() {
 
 	};
 	
-	var ind_names = ["a", "b"];
-
+	var ind_names = ["Part. despesa e encargos educação(%)", "IDEB - 5º ano do ensino fundamental", "IDEB - 9º ano do ensino fundamental", 
+"Taxa de analfabetismo", "Taxa de atendimento escolar", "Taxa abandono total - fundamental(%)", "Taxa de abandono - ensino médio(%)",
+"Taxa aprovação total - fundamental(%)", "Taxa de aprovação - ensino médio (%)", "Percentual docentes formação superior(%)", 
+"Percentual de docentes temporários", "Índice precariedade infraestrutura", "Razão aluno por docente", "Despesa educação aluno", "Índice eficiência educação básica"];
+	 
 	var div_buttons = d3.select("#div_indicador_options");
 	
-	//fix me organizar botao com indicador
-	div_buttons.selectAll("button").data(ind_names).enter().append("button").text(ind_names).attr("value","Sort!").on("click", function() {
+	
+	div_buttons.selectAll("input").data(ind_names).enter().append("input").attr("type","button").attr("class","button").attr("value", function (d){return d;});
+	div_buttons.on("click", function() {
 		sortBars();
 	});
-
 };
 
