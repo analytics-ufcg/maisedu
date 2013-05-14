7141c9ce7006a44b2f2c46d0c1104e06aa220b47
Henrique César Florêncio <henriquerzo@gmail.com>
2013-05-14 20:05:49 -0300

    Atualizando indicador.js
    Adicionada as funcionalidades:
    -Carregar os arquivos
    -Selecionar cidades e anos

--- src/edubrasil/WebContent/javascript/indicador.js
+++ src/edubrasil/WebContent/javascript/indicador.js
@@ -1,67 +1,130 @@
+var dataset = [];
+var rawdata = [];
+
 function getMenuOption(sel) {
     var value = sel.options[sel.selectedIndex].value;
-    plotIndicadores("");
-    plotSeries(""); 
-    plotSeries(value); 
-    plotIndicadores(value);   
+	rawdata = dataset.filter(function(i){return i.NOME_MUNICIPIO == value;})
+	
+	
+   // plotIndicadores("");
+   // plotSeries(""); 
+   // plotSeries(value); 
+   // plotIndicadores(value);   
     
-}
+};
 
-function plotIndicadores(cidade) {
+var parseDate = d3.time.format("%Y").parse;	
+
+
+
+function loadData() {
+		d3.csv("data/tabela_com_todos_os_indicadores_selecionados_e_desvios.csv" , function (data){
+			data.forEach(function(d){
+				//d.ANO = parseDate(d.ANO);
+			});
+			dataset = data;
+			
+			myList = d3.selectAll("#myList");
+			myList.selectAll("option").data(dataset).enter().append("option")
+			.attr("value",function(d){return d.NOME_MUNICIPIO;})
+			.attr("label",function(d){return d.NOME_MUNICIPIO;})
+			
+		});
+		d3.csv("data/dicionario.csv" , function (data){			
+			
+		var div_buttons = d3.select("#div_indicador_options");	
+		
+		div_buttons.selectAll("input")
+		.data(data)
+		.enter()
+		.append("input")
+		.attr("type","button")
+		.attr("class","button rightmenuup")
+		.attr("value", function (d){return d.name;})
+		.attr("id", function (d, i){return d.id;})
+		.style("color", "black")
+		.style("background-color", "red")
+		.on("click", function(d) {
+			plotIndicadores(d.id);
+		});			
+		});
+};
+
+function plotIndicadores(indicador) {
+// e se todos forem NAs?
+// 
 
 	//Width and height
 	var w = 600;
 	var h = 250;
 	
-	if(cidade!=""){
-		var dataset = [ Math.floor((Math.random()*20)+1), Math.floor((Math.random()*20)+1), Math.floor((Math.random()*20)+1), Math.floor((Math.random()*20)+1)];
-
-		var xScale = d3.scale.ordinal().domain(d3.range(dataset.length))
+	if(rawdata.length != 0){
+		var maxYear = d3.max(rawdata.filter(function(d){return d[indicador] != "NA";}).map(function(d){return parseInt(d.ANO)}));
+		
+		var currentYearData = rawdata.filter(function(d){return d.ANO == maxYear;})[0];
+		
+		var subset = [10, currentYearData[indicador]];
+		
+		var xScale = d3.scale.ordinal().domain(d3.range(subset.length))
 		.rangeRoundBands([ 0, w ], 0.05);
 	
-		var yScale = d3.scale.linear().domain([ 0, d3.max(dataset) ])
+		var yScale = d3.scale.linear().domain([ 0, d3.max(subset) ])
 		.range([ 0, h ]);
 	
 		//Create SVG element
-		var svgBar = d3.select("#div_indicador").append("svg")
-		.attr("width", w).attr("height", h);
+		var svg = d3.select("#div_indicador").select("svg");
+		
+		if (svg[0][0] == null){
+			//cria novas barras
+			
+			var svg = d3.select("#div_indicador").append("svg").attr("width", w).attr("height", h);
+			
+			svg.selectAll("rect").data(subset).enter().append("rect").attr(
+			"x", function(d, i) {
+				return xScale(i);
+			}).attr("y", function(d) {
+				return h - yScale(d);
+			}).attr("width", xScale.rangeBand()).attr("height", function(d) {
+				return yScale(d);
+			}).attr("fill", function(d) {
+				return "rgb(0, 0, " + (d * 20) + ")";
+			}).on(
+					"mouseover",
+					function(d) {
+
+						//Get this bar's x/y values, then augment for the tooltip
+						var xPosition = parseFloat(d3.select(this).attr("x"))
+						+ xScale.rangeBand() / 2;
+						var yPosition = parseFloat(d3.select(this).attr("y"))
+						/ 2 + h / 2;
+
+						//Update the tooltip position and value
+						d3.select("#tooltip").style("left", xPosition + "px")
+						.style("top", yPosition + "px")
+						.select("#value").text(d);
+
+						//Show the tooltip
+						d3.select("#tooltip").classed("hidden", false);
+
+					}).on("mouseout", function() {
+
+						//Hide the tooltip
+						d3.select("#tooltip").classed("hidden", true);
+
+					}).on("click", function() {
+						//sortBars();
+					});
+			
+		}else{
+			
+			var bars = d3.select("#div_indicador").select("svg").selectAll("rect")
+			.data(subset)
+			.transition()
+			.attr("y", function(d) {
+				return h - yScale(d);
+			});
+		}
 	
-		//Create bars
-		svgBar.selectAll("rect").data(dataset).enter().append("rect").attr(
-				"x", function(d, i) {
-					return xScale(i);
-				}).attr("y", function(d) {
-					return h - yScale(d);
-				}).attr("width", xScale.rangeBand()).attr("height", function(d) {
-					return yScale(d);
-				}).attr("fill", function(d) {
-					return "rgb(0, 0, " + (d * 20) + ")";
-				}).on(
-						"mouseover",
-						function(d) {
-	
-							//Get this bar's x/y values, then augment for the tooltip
-							var xPosition = parseFloat(d3.select(this).attr("x"))
-							+ xScale.rangeBand() / 2;
-							var yPosition = parseFloat(d3.select(this).attr("y"))
-							/ 2 + h / 2;
-	
-							//Update the tooltip position and value
-							d3.select("#tooltip").style("left", xPosition + "px")
-							.style("top", yPosition + "px")
-							.select("#value").text(d);
-	
-							//Show the tooltip
-							d3.select("#tooltip").classed("hidden", false);
-	
-						}).on("mouseout", function() {
-	
-							//Hide the tooltip
-							d3.select("#tooltip").classed("hidden", true);
-	
-						}).on("click", function() {
-							sortBars();
-						});
 		
 		//Define sort order flag
 		var sortOrder = false;
@@ -84,35 +147,10 @@ function plotIndicadores(cidade) {
 				return xScale(i);
 			});
 	
-		};
-		
-		var ind_names = ["Part. despesa e encargos educação(%)", "IDEB - 5º ano do ensino fundamental", "IDEB - 9º ano do ensino fundamental", 
-	"Taxa de analfabetismo", "Taxa de atendimento escolar", "Taxa abandono total - fundamental(%)", "Taxa de abandono - ensino médio(%)",
-	"Taxa aprovação total - fundamental(%)", "Taxa de aprovação - ensino médio (%)", "Percentual docentes formação superior(%)", 
-	"Percentual de docentes temporários", "Índice precariedade infraestrutura", "Razão aluno por docente", "Despesa educação aluno", "Índice eficiência educação básica"];
-		 
-		var div_buttons = d3.select("#div_indicador_options");
-		
-		
-		div_buttons.selectAll("input")
-		.data(ind_names)
-		.enter()
-		.append("input")
-		.attr("type","button")
-		.attr("class","button rightmenuup")
-		.attr("value", function (d){return d;})
-		.attr("id", function (d, i){return "indicador_button_" + i;})
-		.style("color", "black")
-		.style("background-color", "red");
-		
-		div_buttons.on("click", function() {
-			sortBars();
-		});
-		
+		};		
 	}else{
 		d3.select("svg").remove();
 	}
-	
 	
 };
 
