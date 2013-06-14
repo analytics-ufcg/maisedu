641770e79b87c66c6184bf10a30e10cc2dfd5484
Rodolfo Moraes Martins <rodolfomoraes1@gmail.com>
2013-06-14 17:34:39 -0300

    cidades parecidas update

--- src/edubrasil/WebContent/javascript/cidades_parecidas.js
+++ src/edubrasil/WebContent/javascript/cidades_parecidas.js
@@ -3,6 +3,7 @@ var rawdata = [];
 var dicionario = [];
 var similares = [];
 
+
 function loadData() {
 	d3.csv("data/tabela_com_todos_os_indicadores_selecionados_e_desvios.csv" , function (data){
 
@@ -28,3 +29,21 @@ function loadData() {
 	d3.csv("data/cidades_semelhantes_nomes.csv", function (data){
 		similares = data;});
 };
+
+
+function plotHistograms(){ //TODO fazer metodo depois que se escolher o grafico para representar
+	var dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19,
+	                14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
+	                24, 18, 25, 9, 3 ];
+	
+	d3.select("plot_imagens").selectAll("div")
+    .data(dataset)
+    .enter()
+    .append("div")
+    .attr("class", "bar")
+    .style("height", function(d) {
+        var barHeight = d * 5;
+        return barHeight + "px";
+    });
+}
+
