b184e7d415f4cae181f542bda0d81053fe68bffb
Iara Ribeiro <Iara.prbr@gmail.com>
2013-06-27 15:53:37 -0300

    US19
    Agregando os valores dos indicadores ao ifdm, receita e numero de
    matriculas

--- src/R/Scripts de extração e filtragem(US14)/agrega_indicadores_de_agrupamento(US14).R
+++ src/R/Scripts de extração e filtragem(US14)/agrega_indicadores_de_agrupamento(US14).R
@@ -5,6 +5,8 @@ require("gdata")
 
 #Caminho do interpretedor perl(arquivos xls precisam dessa lib(gdata))
 perl.path = "C:/strawberry/perl/bin/perl"
+data = read.csv("tabela_com_todos_os_indicadores_selecionados_e_desvios.csv")
+
 
 #Carrega arquivos necess√°rtios
 total.matriculas = read.xls("INDICADOR_219 - Total MatrÌculas.xls",perl=perl.path)
@@ -19,27 +21,27 @@ receita$Valor = gsub(",","",receita$Valor)
 total.matriculas = total.matriculas[total.matriculas$ANO==2011,]
 total.matriculas = total.matriculas[with(total.matriculas,order(NOME_MUNICIPIO)),]
 
+#ordenando os indicadores
+data = data[data$ANO == 2011, ]
+data = data[order(data$NOME_MUNICIPIO), ]
 IFDM = IFDM[order(IFDM$Cidade),]
 receita = receita[order(receita$Muninipio), ]
 #FPM = FPM[order(FPM$Nome.do.Munic√???pio),]
 
-
-
 #Merge dos atributos
 #data = cbind(total.matriculas,IFDM$IFDM, FPM$Coeficiente)
-data = cbind(total.matriculas,IFDM$IFDM)#, receita$Valor)
+data = cbind(data, total.matriculas$INDICADOR_219,IFDM$IFDM)#, receita$Valor)
 data$receita = NA
 
 for(i in 1:nrow(data)){
   nome = as.character(data$NOME_MUNICIPIO[i])
-  print(nome)
   if(nrow(receita[receita$Muninipio == nome,]) > 0){
-    data[i,12] = gsub(",","",receita[receita$Muninipio == nome, ]$Valor)
+    data[i,]$receita = receita[receita$Muninipio == nome, ]$Valor
   }
 }
 
 #colnames(data)[10:12] = c("numero.matriculas", "IFDM", "FPM") 
-colnames(data)[10:12] = c("numero.matriculas", "IFDM", "receita") 
+colnames(data)[42:44] = c("numero.matriculas", "IFDM", "receita") 
 
 #Salvando informa√ß√µes agregadas
 write.csv(data,"numero.matriculas_IFDM_e_receita_agregados.csv",row.names=F, dec = ".")
